83e0af0f5003b460ec47c3cb089999bbc7ff77b0
hy-reza <hy.rezaalfanda@gmail.com>
2022-09-25 17:03:03 +0700

    Truthiness Matcher

---
+++ test/truthiness.test.js
@@ -0,0 +1,13 @@
+test('truthiness test', () => {
+  let value = null
+  expect(value).toBeNull()
+  expect(value).toBeDefined()
+  expect(value).toBeFalsy()
+
+  value = undefined
+  expect(value).toBeUndefined()
+  expect(value).toBeFalsy()
+
+  value = "Handy"
+  expect(value).toBeTruthy()
+})
