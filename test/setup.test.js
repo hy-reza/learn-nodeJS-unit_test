9a8b6f4e15415c6d92906000e7f301df2a9b2b0f
hy-reza <hy.rezaalfanda@gmail.com>
2022-09-25 19:38:04 +0700

    setup before & after function for each & all test done

--- test/setup.test.js
+++ test/setup.test.js
@@ -1,9 +1,19 @@
-//this function will run before each test 
+//this function will run before all test done
+beforeAll(() => {
+  console.info('Before All Test')
+})
+
+//this function will run after all test done
+afterAll(() => {
+  console.info('After All Test')
+})
+
+//this function will run before each test done
 beforeEach(() => {
   console.info('Before Test')
 })
 
-//this function will run after each test 
+//this function will run after each test done
 afterEach(() => {
   console.info('After Test')
 })
