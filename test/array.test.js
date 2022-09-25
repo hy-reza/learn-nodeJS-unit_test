0ff9b19673322d7653d0255a37ce48d6dabefa5e
hy-reza <hy.rezaalfanda@gmail.com>
2022-09-25 17:22:05 +0700

    array testing

---
+++ test/array.test.js
@@ -0,0 +1,8 @@
+test('Array testing', () => {
+  let arr = ['Handy', 'Reza', 'Alfanda']
+  //using toBe check
+  expect(arr).toContain('Handy', 'Reza')
+  //using toEqual check
+  arr = [{name : "reza"}, {name: "cholid"}, {name : "ahyar"}]
+  expect(arr).toContainEqual({name : 'reza'})
+})
