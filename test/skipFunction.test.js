6fbff3a9add5efcbe7eb780dd21719ba29d42456
hy-reza <hy.rezaalfanda@gmail.com>
2022-09-25 23:55:04 +0700

    Work with skip() function

---
+++ test/skipFunction.test.js
@@ -0,0 +1,6 @@
+//skip() function
+it('Test - 1', () => {console.info('Test - 1')})
+it('Test - 2', () => {console.info('Test - 2')})
+it.skip('Test - 3', () => {console.info('Test - 3')})
+it('Test - 4', () => {console.info('Test - 4')})
+it('Test - 5', () => {console.info('Test - 5')})
