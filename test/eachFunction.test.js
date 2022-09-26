1bd789024093f8e1547e3f4cc285dd5a4cd04f7d
hy-reza <hy.rezaalfanda@gmail.com>
2022-09-26 07:16:37 +0700

    Work with each() function to handle duplicated test code

--- test/eachFunction.test.js
+++ test/eachFunction.test.js
@@ -1,5 +1,6 @@
 // each() function use to avoid duplicate test code
 import { sum } from "../src/sum.js";
+//first way (using nested array)
 describe("When the sum function is called", () => {
   const table = [
     [30, 15, 15],
@@ -9,6 +10,21 @@ describe("When the sum function is called", () => {
   it.each(table)(
     "Should return %i if the invoke is sum(%i, %i)",
     (expected, a, b) => {
+      expect(sum(a, b)).toBe(expected);
+    }
+  );
+});
+
+// second way (using object destructuring)
+describe("When the sum function is called", () => {
+  const table = [
+    {expected: 30, a: 15, b: 15},
+    {expected: 60, a: 30, b: 30},
+    {expected: 120, a: 60, b: 60},
+  ];
+  it.each(table)(
+    "Should return %expected if the invoke is sum(%a, %b)",
+    ({expected, a, b}) => {
       expect(sum(a, b)).toBe(expected);
     }
   );
